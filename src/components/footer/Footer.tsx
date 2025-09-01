8b954aa8e0dbe593ba7013983c38d933b4dd8b23
Amanda <amandac@genstudents.org>
2025-09-01 00:37:13 -0300

    adiciona estilização

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,32 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogoIcon, GithubLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
     return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+            <>
+                <div className="flex justify-center bg-indigo-900 text-white">
+                    <div className="container flex flex-col items-center py-4">
+                        <p className='text-xl font-bold'>Blog Pessoal Generation | Copyright: {data}</p>
+                        <p className='text-lg'>Acesse nossas redes sociais</p>
+                        <div className='flex gap-2'>
+                            <a href="https://www.linkedin.com/in/amanda-cruz17">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                            </a>
+
+                            <a href="https://github.com/mandirodrigues">
+                            <GithubLogoIcon size={48} weight='bold' />
+                            </a>
+
+                            <a href="https://www.facebook.com/amanda.rodrigues.33449138">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        );
 }
 
 export default Footer
