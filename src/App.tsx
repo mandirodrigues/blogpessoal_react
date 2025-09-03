08211b99b2d600be9dc7e59ebfe38073cdee37db
Amanda <amandac@genstudents.org>
2025-09-02 21:03:14 -0300

    adicionando CRUD tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/login/Login';
 import { AuthProvider } from './contexts/AuthContext';
+import FormTema from './components/tema/formtema/FormTema';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
 
 function App() {
   return (
@@ -17,6 +20,10 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path='/home' element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
